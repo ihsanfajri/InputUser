5438b33bdb0fad2cf2a0faa7eec27cabb379519c
ihsan Fajri <ihsanfajri@gmail.com>
2021-06-04 16:28:36 +0700

    Push

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -3,26 +3,15 @@ import '../data/Data.css'
 import React, { Component } from 'react'
 import axios from 'axios';
 export class data extends Component {
-    state = {
-        persons: []
-    }
-
-    componentDidMount() {
-        axios.get(`http://127.0.0.1:8000/api/person`)
-            .then(res => {
-                const persons = res.data;
-                this.setState({ persons });
-            })
-    }
     render() {
         return (
             <div className="container-data">
                 <h2>Data User</h2>
 
                 <div className="cont-data">
-                    { this.state.persons.map(person => <p className="text">{person.name}</p>)}
-                    { this.state.persons.map(person => <p className="text">{person.age}</p>)}
-                    { this.state.persons.map(person => <p className="text">{person.city}</p>)}
+                    <p className="text">Name</p>
+                    <p className="text">Age</p>
+                    <p className="text">City</p>
                 </div>
             </div>
         )
